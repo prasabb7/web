ef75e8d537e53a1c5f20c60214c94bf934d343ee
jayg <jjgelinas@gmail.com>
2024-06-14 12:37:03 -0400

    UI updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const contactForm = document.querySelector("form");
+    const contactForm = document.querySelector("form")
     
     contactForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevents the default form submission behavior
+        event.preventDefault() // Prevents the default form submission behavior
         
         // Display a simple confirmation message
-        alert("Thank you for your message. We will get back to you soon!");
+        alert("Thank you for your message. We will get back to you soon!")
         
         // Optionally, reset the form fields
-        contactForm.reset();
-    });
-});
+        contactForm.reset()
+    })
+})
